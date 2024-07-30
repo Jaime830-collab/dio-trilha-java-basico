309516a8e5c5160096289058e9f5e9b0830aeb0d
jaime <jaimevaz75@gmail.com>
2024-07-30 12:02:40 -0300

    atualizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+        int numConta = 001;
+        int saldo = 0;
+        
+        System.out.println("Informe seu nome completo: ");
+        String novoUsuario = sc.next();
+
+        System.out.println("Bem-vindo: "+ novoUsuario);
+        System.out.println("Número da conta: " +numConta);
+        System.out.println("Saldo: "+ saldo);
+
+        
+        sc.close();
     }
 }
